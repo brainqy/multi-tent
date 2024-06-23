6ec0f744ff81679b77c548ebfb4f9742ccb5838e
MyGuru <32195301+myprivateguru@users.noreply.github.com>
2024-06-23 17:22:03 +0530

    sample controller

---
+++ src/main/java/com/yash/ytms/controller/HelloController.java
@@ -0,0 +1,24 @@
+package com.yash.ytms.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * Description of the class or file.
+ *
+ * @author Dnyaneshwar Somwanshi
+ * @version 1.0
+ * @project multi-tent
+ * @since 23-06-2024
+ */
+
+@RestController
+@RequestMapping
+public class HelloController {
+    @GetMapping("/hello")
+    public  String getHello(){
+        return  "hello User";
+    }
+
+}
