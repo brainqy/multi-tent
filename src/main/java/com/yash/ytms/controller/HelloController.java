6b34848845824a482f077c90db35ad3210fdfab8
MyGuru <32195301+myprivateguru@users.noreply.github.com>
2024-06-23 18:33:25 +0530

    hello ji

--- src/main/java/com/yash/ytms/controller/HelloController.java
+++ src/main/java/com/yash/ytms/controller/HelloController.java
@@ -17,6 +17,6 @@ import org.springframework.web.bind.annotation.RestController;
 public class HelloController {
     @GetMapping("/hello")
     public String getHello(){
-        return  "Hello User";
+        return  "Hello User in branch";
     }
 }
